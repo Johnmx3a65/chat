2b877f14789d0a7b1a038acb9f2bfb731b00498a
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2021-02-24 18:50:29 +0200

    lesson 21 {factoring dialogs page}

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import dialogs_css from './Dialogs.module.css'
+import DialogsCss from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={DialogsCss.dialogs}>
+            <div className={DialogsCss.dialogs_items}>
+                <div className={DialogsCss.dialog + ' ' + DialogsCss.active}>
+                    Ivan
+                </div>
+                <div className={DialogsCss.dialog}>
+                    Alex
+                </div>
+            </div>
+            <div className={DialogsCss.messages}>
+                <div className={DialogsCss.message}>Hi</div>
+                <div className={DialogsCss.message}>How are you?</div>
+                <div className={DialogsCss.message}>Yo</div>
+            </div>
         </div>
     );
 }
